cef617d7bb7f18db08471337b8b5bcc80a81e079
DykyiV <139080679+DykyiV@users.noreply.github.com>
2026-04-21 22:13:48 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Продаж квитків Grandes Tour</h1>
+      <p>Пошук рейсів скоро буде тут</p>
+    </div>
+  );
+}
