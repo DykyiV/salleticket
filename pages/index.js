7862f7af9382339adca90b936d2cfdb885143193
DykyiV <139080679+DykyiV@users.noreply.github.com>
2026-04-21 22:16:41 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 export default function Home() {
   return (
     <div style={{ padding: 40 }}>
-      <h1>Продаж квитків Grandes Tour</h1>
-      <p>Пошук рейсів скоро буде тут</p>
+      <h1>Асоль Бус</h1>
+      <h2>Продаж квитків онлайн</h2>
+
+      <div style={{ marginTop: 20 }}>
+        <input placeholder="Звідки" />
+        <input placeholder="Куди" style={{ marginLeft: 10 }} />
+        <input type="date" style={{ marginLeft: 10 }} />
+        <button style={{ marginLeft: 10 }}>Знайти рейс</button>
+      </div>
     </div>
   );
 }
